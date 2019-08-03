89615c3963ef4e18bdd0999d358970562a9218d2
Doodge Matvey <Matveyproo1000@gmail.com>
2019-08-03 22:25:19 +0300

    initial commit
    Portf

---
+++ js/main.js
@@ -0,0 +1,50 @@
+//first-image
+$(function(){
+    $('.image-portfolio-first').hover(function(){
+      $('.image-portfolio-first').addClass('image-hover');
+      $('.image-portfolio-first').removeClass('image-leave');
+     },
+    function(){
+      $('.image-portfolio-first').addClass('image-leave');
+      $('.image-portfolio-first').removeClass('image-hover');
+     });
+});
+//sec-image
+$(function(){
+    $('.image-portfolio-sec').hover(function(){
+      $('.image-portfolio-sec').addClass('image-hover');
+      $('.image-portfolio-sec').removeClass('image-leave');
+     },
+    function(){
+      $('.image-portfolio-sec').addClass('image-leave');
+      $('.image-portfolio-sec').removeClass('image-hover');
+     });
+});
+
+var lastScrollTop = 0;
+
+window.addEventListener("scroll", function(){  
+   var st = window.pageYOffset || document.documentElement.scrollTop;  
+   if (st > lastScrollTop){
+        document.getElementById("header").style.top = "-10%";
+   } 
+   else {
+        document.getElementById("header").style.top = "0";
+        
+   }
+   lastScrollTop = st;
+}, false);
+
+
+$(function() {
+    $(window).scroll(function() {
+        if($(this).scrollTop() != 0 ) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+        });
+        $('#toTop').click(function() {
+        $('body,html').animate({scrollTop:0},800);
+    });
+});
